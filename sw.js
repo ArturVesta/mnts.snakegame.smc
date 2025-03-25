6650cfe6cd99f1a33ffcfbdcbbf3391fd4082606
ArturVesta <meirfons2@gmail.com>
2025-03-26 00:51:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/mnts.snakegame.smc';
 var APP_PREFIX = 'gppwa_';
-let VERSIONNAME = "0.3.8";
+let VERSIONNAME = "0.3.9";
 var VERSION = VERSIONNAME;
 var CACHE_NAME = VERSIONNAME;
 var URLS = [
