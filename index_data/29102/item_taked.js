6d298e314bb227c6f767baf182f4ef76da06e938
ArturVesta <meirfons2@gmail.com>
2025-04-05 11:43:00 +0300

    Delete index_data directory

--- index_data/29102/item_taked.js
+++
@@ -1,6 +0,0 @@
-function playCollectItem() {
-  if(localStorage.getItem("foodStyle") === "imgMeow" || localStorage.getItem("foodStyle") === "imgGold" || localStorage.getItem("foodStyle") === "imgAlphacoin" || localStorage.getItem("foodStyle") === "imgCrystal" || localStorage.getItem("foodStyle") === "imgTicket" || localStorage.getItem("foodStyle") === "imgCticket" || localStorage.getItem("foodStyle") === "imgPticket") {
-    document.getElementById("itemCollected").muted = false;
-    document.getElementById("itemCollected").play();
-  }
-};
