8893e1da2fab18150583fea3d6e28094537e57c9
ArturVesta <meirfons2@gmail.com>
2025-04-05 12:30:24 +0300

    Delete shortcut_loader_OneDrive.bat

--- shortcut_loader_OneDrive.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo [InternetShortcut]>> %UserProfile%/OneDrive/Desktop/SMC.url
-echo URL=%cd%\index.html>> %UserProfile%/OneDrive/Desktop/SMC.url
-echo IDList=>> %UserProfile%/OneDrive/Desktop/SMC.url
-echo IconFile=%cd%\icons.dll>> %UserProfile%/OneDrive/Desktop/SMC.url
-echo IconIndex=0>> %UserProfile%/OneDrive/Desktop/SMC.url
-echo HotKey=0>> %UserProfile%/OneDrive/Desktop/SMC.url
