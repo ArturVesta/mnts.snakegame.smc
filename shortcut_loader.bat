66dc02fc1bec503c0829115c95fdb9bb98c5fefa
ArturVesta <meirfons2@gmail.com>
2025-04-05 12:29:11 +0300

    Delete shortcut_loader.bat

--- shortcut_loader.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo [InternetShortcut]>> %UserProfile%/Desktop/SMC.url
-echo URL=%cd%\index.html>> %UserProfile%/Desktop/SMC.url
-echo IDList=>> %UserProfile%/Desktop/SMC.url
-echo IconFile=%cd%\icons.dll>> %UserProfile%/Desktop/SMC.url
-echo IconIndex=0>> %UserProfile%/Desktop/SMC.url
-echo HotKey=0>> %UserProfile%/Desktop/SMC.url
